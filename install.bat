6f3b7c8a65e8d6b124f805224c501294a1c8270a
Ahmed Abousetta <ahmadabousetta@gmail.com>
2023-03-22 11:03:11 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 @REM We use py assuming a windows environment, if you are on linux or mac, use python instead of py
 py -m venv .venv
 call .venv\Scripts\activate.bat
-py -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 @REM Press any key to continue message
 pause
