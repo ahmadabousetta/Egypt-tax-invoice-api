dd082664b2cebaf6a1ccbb78ed989318bde11242
Ahmed Abousetta <ahmadabousetta@gmail.com>
2023-04-01 17:12:26 +0200

    allow spaces in folder path in c# signer bat file

--- c#_signer/SubmitInvoices.bat
+++ c#_signer/SubmitInvoices.bat
@@ -6,7 +6,8 @@ set app_dir=%~dp0
 
 cd /d "%app_dir%"
 
-call publish\EInvoicingSigner.exe  %app_dir% 60906090 "Egypt Trust Sealing CA"
+@REM use ~0,-1 to remove the trailing slash in folder path which results in escaping the " .
+call publish\EInvoicingSigner.exe  "%app_dir:~0,-1%" 60906090 "Egypt Trust Sealing CA"
 
 @REM uncomment the below line if you need to pause the cmd screen after it finishes to debug or read output.
 @REM PAUSE
