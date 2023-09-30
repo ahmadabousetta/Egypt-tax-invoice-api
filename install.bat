9dadfb4cb7c0037c0c0a61de16854f85666af34a
Ahmed Abousetta <ahmadabousetta@gmail.com>
2023-10-01 00:13:42 +0300

    use python command instead of py

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 
-@REM We use py assuming a windows environment, if you are on linux or mac, use python instead of py
-py -m venv .venv
+@REM you may need to use py instead of python
+python -m venv .venv
 call .venv\Scripts\activate.bat
 python -m pip install -r requirements.txt
 
