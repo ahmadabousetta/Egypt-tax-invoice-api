097a848aa49345ab629e5650185d9c6a41068a7c
Ahmed Abousetta <ahmadabousetta@gmail.com>
2025-02-20 13:04:15 +0200

    Fixed a missing "

--- c#_signer/SubmitInvoices.bat
+++ c#_signer/SubmitInvoices.bat
@@ -1,7 +1,7 @@
 set "app_dir=%~dp0"
 
 @REM Replace 60906090 with your token PIN and "Egypt Trust Sealing CA" with your certificate issuer.
-call "%app_dir%\publish\EInvoicingSigner.exe"  "%app_dir" 60906090 "Egypt Trust Sealing CA"
+call "%app_dir%\publish\EInvoicingSigner.exe"  "%app_dir%" 60906090 "Egypt Trust Sealing CA"
 
 @REM uncomment the below line if you need to pause the cmd screen after it finishes to debug or read output.
 @REM PAUSE
