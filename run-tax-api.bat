88a64e00273b41b3cd613608add81bc2e460db76
Ahmed Abousetta <ahmadabousetta@gmail.com>
2025-02-20 12:59:32 +0200

    Use quotations to fix spaces issues in paths

--- run-tax-api.bat
+++ run-tax-api.bat
@@ -1,8 +1,8 @@
 @REM We differentiate the calling directory from the app directory
 @REM for cases when script is called from a scheduler.
 
-set cwd=%cd%
-set app_dir=%~dp0
+set "cwd=%cd%"
+set "app_dir=%~dp0"
 
 cd /d "%app_dir%"
 
