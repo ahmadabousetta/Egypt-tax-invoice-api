e5aeaf47208e5fc6f50a21c8278ae82ff8721fb8
Ahmed Abousetta <ahmadabousetta@gmail.com>
2025-02-20 12:58:13 +0200

    Simplify calling the app and use quotations to avoid issues with spaces in paths

--- c#_signer/SubmitInvoices.bat
+++ c#_signer/SubmitInvoices.bat
@@ -1,13 +1,7 @@
-@REM We differentiate the calling directory from the app directory
-@REM for cases when script is called from a scheduler.
+set "app_dir=%~dp0"
 
-set cwd=%cd%
-set app_dir=%~dp0
-
-cd /d "%app_dir%"
-
-@REM use ~0,-1 to remove the trailing slash in folder path which results in escaping the " .
-call publish\EInvoicingSigner.exe  "%app_dir:~0,-1%" 60906090 "Egypt Trust Sealing CA"
+@REM Replace 60906090 with your token PIN and "Egypt Trust Sealing CA" with your certificate issuer.
+call "%app_dir%\publish\EInvoicingSigner.exe"  "%app_dir" 60906090 "Egypt Trust Sealing CA"
 
 @REM uncomment the below line if you need to pause the cmd screen after it finishes to debug or read output.
 @REM PAUSE
